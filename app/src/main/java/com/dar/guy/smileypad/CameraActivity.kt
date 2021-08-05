5c5492be0aa61d1762aa01ad499bc2ee383e8a61
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2021-08-05 22:05:51 +0300

    add camera activity

---
+++ app/src/main/java/com/dar/guy/smileypad/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.dar.guy.smileypad
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class CameraActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_camera)
+    }
+}
