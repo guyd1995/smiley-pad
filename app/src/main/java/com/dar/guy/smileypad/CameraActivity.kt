3fbbf66014021ae18ebc359e9674564462f6417a
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2021-08-06 15:03:41 +0300

    add camera capability

--- app/src/main/java/com/dar/guy/smileypad/CameraActivity.kt
+++ app/src/main/java/com/dar/guy/smileypad/CameraActivity.kt
@@ -1,12 +1,35 @@
 package com.dar.guy.smileypad
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+
 
 class CameraActivity : AppCompatActivity(){
+    var REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_camera)
+    }
+
+    fun startCamera(view: View){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+        }
     }
 }
