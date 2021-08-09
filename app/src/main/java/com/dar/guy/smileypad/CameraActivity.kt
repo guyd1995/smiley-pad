b9724e9902e46bd250655d63619df26b1781ad8f
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2021-08-09 17:51:09 +0300

    add models

--- app/src/main/java/com/dar/guy/smileypad/CameraActivity.kt
+++ app/src/main/java/com/dar/guy/smileypad/CameraActivity.kt
@@ -1,24 +1,28 @@
 package com.dar.guy.smileypad
 
+import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.widget.Toast
 
 
 class CameraActivity : AppCompatActivity(){
     var REQUEST_IMAGE_CAPTURE = 1
-
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_camera)
     }
 
-    fun startCamera(view: View){
+    fun startCamera(){
+
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
