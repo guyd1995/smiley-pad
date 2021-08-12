71c4723fc966aee6296d46f49414aa74fdc26e1e
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2021-08-12 15:41:07 +0300

    tidy up and minor fix

--- models/train.py
+++ models/train.py
@@ -74,49 +74,15 @@ class FecDataset(Dataset):
         return len(self.data)
 
 
-class OldFecNet(nn.Module):
-    def __init__(self, dropout_rate=.2):
-        super().__init__()
-        resnet = InceptionResnetV1(pretrained='vggface2').eval()   
-        self.backbone = intermediate_layer_getter(resnet, 'mixed_6a')
-        self.conv = nn.Conv2d(896, 512, (1,1))
-        self.dense_block = _DenseBlock(num_layers=5, num_input_features=512, growth_rate=64, bn_size=4, drop_rate=0)
-        self.avg_pool = nn.AvgPool2d(12)
-        self.dropout = nn.Dropout(dropout_rate)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(832, 512)
-        self.fc2 = nn.Linear(512, 16)
-    
-    def forward(self, x):
-        with torch.no_grad():
-            x = self.backbone(x).detach()
-        x = self.dropout(x)
-        x = self.conv(x)
-        x = F.relu(x)
-        x = self.dense_block(x)
-        x = self.avg_pool(x)
-        x = self.flatten(x)
-        x = self.dropout(x)
-        x = self.fc1(x)
-        x = F.relu6(x)
-        x = self.fc2(x)
-        x = x / F.normalize(x, dim=-1)
-        return x
-
-
 class FecNet(nn.Module):
     def __init__(self, dropout_rate=.2):
         super().__init__()
         self.facenet = self._get_truncated_facenet()
-#         self.conv = nn.Conv2d(25, 3, (3,3))
         self.densenet = DenseNet(block_config=(5,), num_init_features=512, 
                                  growth_rate=64, num_classes=16, 
                                  drop_rate=dropout_rate)
         self.densenet.features[0] = nn.Conv2d(1792, 512, kernel_size=7, stride=2, padding=3, bias=False)
         self.dropout = nn.Dropout(dropout_rate)
-#         self.fc1 = nn.Linear(1024, 512)
-#         self.bn = nn.BatchNorm1d(512)
-#         self.fc2 = nn.Linear(512, 16)
     
     @staticmethod
     def _get_truncated_facenet():
@@ -126,21 +92,12 @@ class FecNet(nn.Module):
         facenet.last_bn = Identity()
         facenet.logits = Identity()
         facenet.requires_grad_(False)
-#         facenet.mixed_7a.register_forward_hook(lambda *x: print(x[1][0].shape))
         return facenet
     
     def forward(self, x):
         x = self.facenet(x)
-#         x = x.reshape((x.shape[0], 25, 32, 56))
-#         x = self.conv(x)
-#         x = F.relu(x)
         x = x.reshape(x.shape[0], 1792, 5, 5)
         x = self.densenet(x)
-#         x = self.dropout(x)
-#         x = self.fc1(x)
-#         x = self.bn(x)
-#         x = F.relu6(x)
-#         x = self.fc2(x)
         x = F.normalize(x, p=2, dim=-1)
         return x
 
